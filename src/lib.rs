68b2bd15c83461ecc0f30dd9e07da0f54cc02a6e
Yuet Shing Feng <fengyuetshing@gmail.com>
2024-01-28 10:25:14 +0800

    added the libraries

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use rpi_build_hat_serial;
+pub use rppal;
+pub use hc_sr04;
