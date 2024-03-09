311050fc09785045ac0f63f6c78aa9b493b9614a
Yuet Shing Feng <fengyuetshing@gmail.com>
2024-03-09 20:56:57 +0800

    added basic functions

--- src/bbr_irseeker.rs
+++ src/bbr_irseeker.rs
@@ -7,22 +7,22 @@ impl Irseeker {
     /// creates a new Irseeker object.
     pub async unsafe fn new() -> Result<Self> {
         let mut ir_i2c = i2c::I2c::new()?;
-        ir_i2c.set_slave_address(0x8);
+        let _ = ir_i2c.set_slave_address(0x8);
         Ok(Self { ir: ir_i2c })
     }
     pub async unsafe fn get_direction(&mut self) -> u8 {
         let mut buf: [u8; 2] = [0, 0];
-        self.ir.read(&mut buf);
+        let _ = self.ir.read(&mut buf);
         buf[0]
     }
     pub async unsafe fn get_strength(&mut self) -> u8 {
         let mut buf: [u8; 2] = [0, 0];
-        self.ir.read(&mut buf);
+        let _ = self.ir.read(&mut buf);
         buf[1]
     }
     pub async unsafe fn get_both(&mut self) -> [u8; 2] {
         let mut buf: [u8; 2] = [0, 0];
-        self.ir.read(&mut buf);
+        let _ = self.ir.read(&mut buf);
         buf
     }
 }
--- src/hc_sr04.rs
+++ src/hc_sr04.rs
@@ -28,4 +28,29 @@ impl Mpu6050 {
         i.set_slave_address(0x68)?;
         Ok(Self { i2c: i })
     }
+    pub fn new_with_addr(addr: u8) -> Result<Self, Mpu6050Error> {
+        let mut i = i2c::I2c::new()?;
+        i.set_slave_address(addr.into())?;
+        Ok(Self { i2c: i })
+    }
+
+    pub fn write_byte(&mut self, reg: u8, byte: u8) -> Result<(), Mpu6050Error> {
+        self.i2c
+            .write(&[reg, byte])
+            .map_err(Mpu6050Error::I2cError)?;
+        Ok(())
+    }
+    pub fn read_byte(&mut self, reg: u8) -> Result<u8, Mpu6050Error> {
+        let mut byte: [u8; 1] = [0; 1];
+        self.i2c
+            .write_read(&[reg], &mut byte)
+            .map_err(Mpu6050Error::I2cError)?;
+        Ok(byte[0])
+    }
+    pub fn read_bytes(&mut self, reg: u8, buf: &mut [u8]) -> Result<(), Mpu6050Error> {
+        self.i2c
+            .write_read(&[reg], buf)
+            .map_err(Mpu6050Error::I2cError)?;
+        Ok(())
+    }
 }
