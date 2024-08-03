4246c5fb50c664a2c2b30ed570872748780407fb
Yuet Shing Feng <fengyuetshing@gmail.com>
2024-08-03 14:11:25 +0800

    added compass

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub use rpi_build_hat_serial;
 pub use rppal;
-
+pub use lsm303dlhc;
 pub mod bbr_irseeker;
 
 pub use libm;
@@ -10,3 +10,4 @@ mod bits;
 pub mod device;
 pub mod mpu6050;
 mod omni; // omni library is not done
+pub mod compass;
