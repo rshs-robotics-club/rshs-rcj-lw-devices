d93293f30d76cacb7e48aec55d7ca016fec77b47
Yuet Shing Feng <fengyuetshing@gmail.com>
2024-08-13 16:29:49 +0800

    added mcp3008 library

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,14 @@ pub use rpi_build_hat_serial;
 pub use rppal;
 pub use lsm303dlhc;
 pub mod bbr_irseeker;
-
-pub use libm;
-pub use nalgebra;
-
-mod bits;
 pub mod device;
 pub mod mpu6050;
 pub mod compass;
 pub mod omni;
+mod bits;
+
+
+pub use libm;
+pub use nalgebra;
+pub use mcp3008;
+
