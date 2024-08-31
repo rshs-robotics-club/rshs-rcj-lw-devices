0effae7a62a894810842026e57dbcccd7ceaefe9
Yuet Shing Feng <fengyuetshing@gmail.com>
2024-08-31 14:20:50 +0800

    removed libraries that we dont use anymore

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,10 @@
 pub mod bbr_irseeker;
-mod bits;
-pub mod compass;
-pub mod device;
-pub mod mpu6050;
 pub mod omni;
 
-pub use libm;
-pub use lsm303dlhc;
-pub use mcp3008;
 pub use nalgebra;
 pub use rpi_build_hat_serial;
 pub use rppal;
 pub use pollster;
-pub use hc_sr04;
 pub use xca9548a;
 pub use bno055;
 pub use bno080;
